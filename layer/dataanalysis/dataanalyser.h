65b4b5071e99158b6cde2f253c8a892305f1d6aa
Kevin Meyer <meyer.kevin@mytum.de>
2013-10-29 17:31:19 +0100

    added empty dataanalyser classes.

---
+++ layer/dataanalysis/dataanalyser.h
@@ -0,0 +1,21 @@
+#ifndef ROBOHOCKEY_LAYER_DATAANALYSIS_DATAANALYSER_H
+#define ROBOHOCKEY_LAYER_DATAANALYSIS_DATAANALYSER_H
+
+
+namespace RoboHockey
+{
+namespace Layer
+{
+namespace DataAnalysis
+{
+
+class DataAnalyser
+{
+public:
+	virtual ~DataAnalyser() { }
+
+};
+}
+}
+}
+#endif
---
+++ layer/dataanalysis/dataanalyserstub.h
@@ -0,0 +1,23 @@
+#ifndef ROBOHOCKEY_LAYER_DATAANALYSIS_DATAANALYSERSTUB_H
+#define ROBOHOCKEY_LAYER_DATAANALYSIS_DATAANALYSERSTUB_H
+
+#include "layer/dataanalysis/dataanalyser.h"
+namespace RoboHockey
+{
+namespace Layer
+{
+namespace DataAnalysis
+{
+class DataAnalyserStub:
+		public DataAnalyser
+
+{
+public:
+
+
+};
+}
+}
+}
+
+#endif
