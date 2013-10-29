112142355dd163aee05ac4affac8758679e99dc2
Benedikt Schmidt <benediktibk@aon.at>
2013-10-29 18:00:39 +0100

    fixed possible bug in Lock-Test

--- threading/locktest.cpp
+++ threading/locktest.cpp
@@ -7,9 +7,12 @@ using namespace Threading;
 void LockTest::constructor_validMutex_mutexIsLocked()
 {
 	Mutex mutex;
-	Lock lock(mutex);
 
-	CPPUNIT_ASSERT(mutex.isLocked());
+	{
+		Lock lock(mutex);
+
+		CPPUNIT_ASSERT(mutex.isLocked());
+	}
 }
 
 void LockTest::destructor_empty_mutexIsNotLocked()
