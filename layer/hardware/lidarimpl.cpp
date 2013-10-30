2e244ef6cc5873ce71a62cdfa6fcb0e875427223
Markus Hofbauer <markus.hofbauer@tum.de>
2013-10-30 10:17:32 +0100

    moved playerinclude fro .h into .cpp

--- layer/hardware/lidarimpl.cpp
+++ layer/hardware/lidarimpl.cpp
@@ -1,13 +1,14 @@
 #include "layer/hardware/lidarimpl.h"
+#include <player-3.0/libplayerc++/playerc++.h>
 
 using namespace RoboHockey::Layer::Hardware;
 using namespace PlayerCc;
 
 LidarImpl::LidarImpl(PlayerClient *playerClient) :
-	m_laser(playerClient, 0)
+	m_laser(new LaserProxy(playerClient, 0))
 { }
 
 double LidarImpl::getDistance(unsigned int angle)
 {
-	return m_laser.GetRange(angle);
+	return m_laser->GetRange(angle);
 }
--- layer/hardware/lidarimpl.h
+++ layer/hardware/lidarimpl.h
@@ -2,8 +2,12 @@
 #define ROBOHOCKEY_LAYER_HARDWARE_LIDARIMPL_H
 
 #include "layer/hardware/lidar.h"
-#include <player-3.0/libplayerc++/playerc++.h>
 
+namespace PlayerCc
+{
+	class PlayerClient;
+	class LaserProxy;
+}
 namespace RoboHockey
 {
 namespace Layer
@@ -19,7 +23,7 @@ namespace Hardware
 		virtual double getDistance(unsigned int angle);
 
 	private:
-		PlayerCc::LaserProxy m_laser;
+		PlayerCc::LaserProxy *m_laser;
 	};
 }
 }
