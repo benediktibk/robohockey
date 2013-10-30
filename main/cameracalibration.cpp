f5215d6f59d3aef592731959907675ab4fa260c7
Kevin Meyer <meyer.kevin@mytum.de>
2013-10-30 15:26:19 +0100

    added files for executable calibrateCamera

---
+++ main/cameracalibration.cpp
@@ -0,0 +1,15 @@
+#include <cv.h>
+#include <highgui.h>
+#include <iostream>
+//#include "layer/hardware/cameraimpl.h"
+
+using namespace std;
+using namespace cv;
+//using namespace RoboHockey;
+//using namespace RoboHockey::Layer::Hardware;
+
+int main(int /*argc*/, char **/*argv*/)
+{
+	return 0;
+}
+
