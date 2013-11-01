b268e02b72272fac1d823617f2968cff71d941d4
Benedikt Schmidt <benediktibk@aon.at>
2013-11-01 21:42:12 +0100

    improved test for the lidar

--- layer/dataanalysis/lidartest.cpp
+++ layer/dataanalysis/lidartest.cpp
@@ -1,6 +1,7 @@
 #include "layer/dataanalysis/lidartest.h"
 #include "layer/dataanalysis/lidarimpl.h"
 #include "layer/hardware/lidarmock.h"
+#include "layer/hardware/lidarstub.h"
 #include "common/compare.h"
 
 using namespace RoboHockey::Common;
@@ -20,7 +21,46 @@ void LidarTest::getAllObjects_mockHardwareLidar_atLeastOneCallToGetDistance()
 
 void LidarTest::getAllObjects_oneObjectInFront_onlyObjectIsCorrect()
 {
-	Hardware::LidarMock hardwareLidar;
+	Hardware::LidarStub hardwareLidar(10);
+	hardwareLidar.setValueForAngle(161, 3.04);
+	hardwareLidar.setValueForAngle(162, 3.02);
+	hardwareLidar.setValueForAngle(163, 2.99);
+	hardwareLidar.setValueForAngle(164, 2.97);
+	hardwareLidar.setValueForAngle(165, 2.94);
+	hardwareLidar.setValueForAngle(166, 2.92);
+	hardwareLidar.setValueForAngle(167, 2.89);
+	hardwareLidar.setValueForAngle(168, 2.87);
+	hardwareLidar.setValueForAngle(169, 2.84);
+	hardwareLidar.setValueForAngle(170, 2.81);
+	hardwareLidar.setValueForAngle(171, 2.77);
+	hardwareLidar.setValueForAngle(172, 2.74);
+	hardwareLidar.setValueForAngle(173, 2.71);
+	hardwareLidar.setValueForAngle(174, 2.68);
+	hardwareLidar.setValueForAngle(175, 2.65);
+	hardwareLidar.setValueForAngle(176, 2.62);
+	hardwareLidar.setValueForAngle(177, 2.59);
+	hardwareLidar.setValueForAngle(178, 2.56);
+	hardwareLidar.setValueForAngle(179, 2.53);
+	hardwareLidar.setValueForAngle(180, 2.5);
+	hardwareLidar.setValueForAngle(181, 2.53);
+	hardwareLidar.setValueForAngle(182, 2.56);
+	hardwareLidar.setValueForAngle(183, 2.59);
+	hardwareLidar.setValueForAngle(184, 2.62);
+	hardwareLidar.setValueForAngle(185, 2.65);
+	hardwareLidar.setValueForAngle(186, 2.68);
+	hardwareLidar.setValueForAngle(187, 2.71);
+	hardwareLidar.setValueForAngle(188, 2.74);
+	hardwareLidar.setValueForAngle(189, 2.77);
+	hardwareLidar.setValueForAngle(190, 2.81);
+	hardwareLidar.setValueForAngle(191, 2.84);
+	hardwareLidar.setValueForAngle(192, 2.87);
+	hardwareLidar.setValueForAngle(193, 2.89);
+	hardwareLidar.setValueForAngle(194, 2.92);
+	hardwareLidar.setValueForAngle(195, 2.94);
+	hardwareLidar.setValueForAngle(196, 2.97);
+	hardwareLidar.setValueForAngle(197, 2.99);
+	hardwareLidar.setValueForAngle(198, 3.02);
+	hardwareLidar.setValueForAngle(199, 3.04);
 	LidarImpl lidar(hardwareLidar);
 
 	LidarObjects allObjects = lidar.getAllObjects(Point(1, 1), 0);
@@ -28,6 +68,6 @@ void LidarTest::getAllObjects_oneObjectInFront_onlyObjectIsCorrect()
 	vector<LidarObject> objects = allObjects.getObjectsWithDistanceBelow(5);
 	CPPUNIT_ASSERT_EQUAL((size_t)1, objects.size());
 	LidarObject object = objects.front();
-	CPPUNIT_ASSERT_DOUBLES_EQUAL(0.5, object.getDiameter(), 0.01);
+	CPPUNIT_ASSERT_DOUBLES_EQUAL(1, object.getDiameter(), 0.01);
 	CPPUNIT_ASSERT(Point(1, 4).fuzzyEqual(object.getPosition(), 0.01));
 }
