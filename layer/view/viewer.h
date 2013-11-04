1ed846d228949c4527816d0dd6df1bf417a658a7
Florian Spinnler <a25@o2mail.de>
2013-11-04 01:27:33 +0100

    implemented view

---
+++ layer/view/viewer.h
@@ -0,0 +1,23 @@
+#ifndef ROBOHOCKEY_LAYER_VIEW_VIEWER_H
+#define ROBOHOCKEY_LAYER_VIEW_VIEWER_H
+
+
+#include <QDialog>
+
+namespace Ui {
+class viewer;
+}
+
+class viewer : public QDialog
+{
+    Q_OBJECT
+    
+public:
+    explicit viewer(QWidget *parent = 0);
+    ~viewer();
+    
+private:
+    Ui::viewer *ui;
+};
+
+#endif // VIEWER_H
