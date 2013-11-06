af9dc74ead25603c32877d87354dafb45e4f345e
Markus Hofbauer <markus.hofbauer@tum.de>
2013-11-06 10:27:08 +0100

    coment on operation

--- layer/dataanalysis/cameraimpl.cpp
+++ layer/dataanalysis/cameraimpl.cpp
@@ -27,7 +27,7 @@ Mat CameraImpl::getFilteredFrame(unsigned int threshold)
 	Mat frame = getSmoothFrame();
 	Mat filteredFrame = frame;
 
-	//threshold used for building
+	//threshold used for building but will be used for parameter 3 and 4
 	threshold = threshold + 1;
 	cvPyrMeanShiftFiltering(&frame, &filteredFrame, 10, 35, 3);
 	return filteredFrame;
