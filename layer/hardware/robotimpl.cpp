8ca76fa3f4f83712a018add7795cbf717fb02b60
Benedikt Schmidt <benediktibk@aon.at>
2013-11-06 18:12:32 +0100

    fixed startup of the lidar

--- layer/hardware/robotimpl.cpp
+++ layer/hardware/robotimpl.cpp
@@ -17,6 +17,7 @@ RobotImpl::RobotImpl()
 	m_odometry = new OdometryImpl(m_playerClient);
 	m_engine = new EngineImpl(m_playerClient);
 
+	updateSensorData();
 	while (!m_lidar->isValid())
 	{
 		usleep(200000);
