79c8cbed27913bdaa2143141114e475f357c7b4c
Benedikt Schmidt <benediktibk@aon.at>
2013-11-07 18:26:34 +0100

    integrated the robot driver into the remote control

--- main/remotecontrol.cpp
+++ main/remotecontrol.cpp
@@ -1,14 +1,25 @@
 #include <QtGui/QApplication>
 #include "layer/view/viewer.h"
-//#include "layer/view/model.h"
+#include "layer/view/model.h"
+#include "layer/view/robotdriver.h"
+#include "layer/view/robotdriverloop.h"
+#include "layer/autonomous/robotimpl.h"
+#include "layer/dataanalysis/dataanalyserimpl.h"
+#include "layer/hardware/robotimpl.h"
 
+using namespace RoboHockey::Layer;
 using namespace RoboHockey::Layer::View;
 
 int main(int argc, char **argv)
 {
 	QApplication application(argc, argv);
 	Viewer view;
-
+	Model model;
+	Hardware::Robot *hardwareRobot = new Hardware::RobotImpl();
+	DataAnalysis::DataAnalyser *dataAnalyser = new DataAnalysis::DataAnalyserImpl(hardwareRobot);
+	Autonomous::RobotImpl autonomousRobot(dataAnalyser);
+	RobotDriver robotDriver(autonomousRobot, model);
+	RobotDriverLoop loop(robotDriver);
 	view.show();
 	return application.exec();
 }
