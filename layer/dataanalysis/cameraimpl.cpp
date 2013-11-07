653920f4e8a2caa3b99e6e7ad13a65e19bd392be
Markus Hofbauer <markus.hofbauer@tum.de>
2013-11-07 12:24:41 +0100

    edit pyrfilter

--- layer/dataanalysis/cameraimpl.cpp
+++ layer/dataanalysis/cameraimpl.cpp
@@ -22,13 +22,11 @@ Mat CameraImpl::getSmoothFrame()
 	return smoothFrame;
 }
 
-Mat CameraImpl::getFilteredFrame(unsigned int threshold)
+Mat CameraImpl::getFilteredFrame()
 {
 	Mat frame = getSmoothFrame();
 	Mat filteredFrame = frame;
 
-	//threshold used for building but will be used for parameter 3 and 4
-	threshold = threshold + 1;
 	cvPyrMeanShiftFiltering(&frame, &filteredFrame, 10, 35, 3);
 	return filteredFrame;
 }
