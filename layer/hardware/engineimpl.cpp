300c4e0e8b64ee3e392eba2a3d9d3f6cfa0b72ee
Benedikt Schmidt <benediktibk@aon.at>
2013-11-08 14:32:46 +0100

    implemented tresholds for the speed and rotation

--- layer/hardware/engineimpl.cpp
+++ layer/hardware/engineimpl.cpp
@@ -18,6 +18,18 @@ EngineImpl::~EngineImpl()
 
 void EngineImpl::setSpeed(double magnitude, double rotation)
 {
+	// tresholds for the speed magnitude
+	if (magnitude > 0.5)
+		magnitude = 0.5;
+	else if (magnitude < -0.5)
+		magnitude = -0.5;
+
+	// tresholds for the rotation
+	if (rotation > M_PI)
+		rotation = M_PI;
+	else if (rotation < (-1)*M_PI)
+		rotation = (-1)*M_PI;
+
 	return m_engine->SetSpeed(magnitude, rotation);
 }
 
