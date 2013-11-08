88f72725921b32c00dd1ad28a88f1b748bfa30e9
Markus Hofbauer <markus.hofbauer@tum.de>
2013-11-08 17:09:11 +0100

    filled cameraobject with content

--- layer/dataanalysis/cameraobject.cpp
+++ layer/dataanalysis/cameraobject.cpp
@@ -1,7 +1,21 @@
 #include "layer/dataanalysis/cameraobject.h"
 
+using namespace RoboHockey::Common;
 using namespace RoboHockey::Layer::DataAnalysis;
+using namespace cv;
 
-CameraObject::CameraObject()
+CameraObject::CameraObject(ColorType color, Rect position) :
+	m_color(color),
+	m_position(position)
 {
+}
+
+Rect CameraObject::getImagePosition() const
+{
+	return m_position;
+}
+
+ColorType CameraObject::getColorType() const
+{
+	return m_color;
 }
--- layer/dataanalysis/cameraobject.h
+++ layer/dataanalysis/cameraobject.h
@@ -1,6 +1,9 @@
 #ifndef ROBOHOCKEY_LAYER_DATAANALYSIS_CAMERAOBJECT_H
 #define ROBOHOCKEY_LAYER_DATAANALYSIS_CAMERAOBJECT_H
 
+#include <opencv2/opencv.hpp>
+#include "common/colorType.h"
+
 namespace RoboHockey
 {
 namespace Layer
@@ -10,7 +13,14 @@ namespace DataAnalysis
 	class CameraObject
 	{
 	public:
-		CameraObject();
+		CameraObject(Common::ColorType color, cv::Rect position);
+
+		cv::Rect getImagePosition() const;
+		Common::ColorType getColorType() const;
+
+	private:
+		Common::ColorType m_color;
+		cv::Rect m_position;
 	};
 }
 }
