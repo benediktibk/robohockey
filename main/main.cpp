e234c5901ec1238b193365f64c5b105b52c062e0
Benedikt Schmidt <benediktibk@aon.at>
2013-11-12 19:23:36 +0100

    implemented possibility to close the robot with key press on q

--- main/main.cpp
+++ main/main.cpp
@@ -5,10 +5,41 @@
 #include "layer/strategy/initialstate.h"
 #include "common/watch.h"
 #include <iostream>
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 using namespace RoboHockey::Common;
 using namespace RoboHockey::Layer;
 using namespace std;
+
+char kbhit(void)
+{
+  struct termios oldt, newt;
+  char key;
+  int oldf;
+
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+  key = getchar();
+
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+  if(key != EOF)
+  {
+	ungetc(key, stdin);
+	return key;
+  }
+
+  return 0;
+}
 
 int main(int argc, char **argv)
 {
@@ -23,7 +54,6 @@ int main(int argc, char **argv)
 	}
 
 	cout << "starting the robot" << endl;
-	cout << "program can be closed with 'q'" << endl;
 
 	Hardware::Robot *hardwareRobot = new Hardware::RobotImpl(playerServer);
 	DataAnalysis::DataAnalyser *dataAnalyser = new DataAnalysis::DataAnalyserImpl(hardwareRobot);
@@ -33,11 +63,18 @@ int main(int argc, char **argv)
 	double lastTime = watch.getTime();
 	const double maximumLoopTime = 0.1;
 
+	cout << "program can be closed with 'q'" << endl;
+
 	while(running)
 	{
 		stateMachine.update();
-		//! @todo check keyboard asynchronous for q
-		running = true;
+		char key = kbhit();
+
+		if (key == 'q')
+			running = false;
+		else
+			running = true;
+
 		double currentTime = watch.getTime();
 		double timeDifference = currentTime - lastTime;
 		lastTime = currentTime;
