078b1b9be09e969d4652b14fe70e3df1441554d0
Markus Hofbauer <markus.hofbauer@tum.de>
2013-11-14 14:29:42 +0100

    add better method for counting white pixel

--- layer/dataanalysis/cameraimpl.cpp
+++ layer/dataanalysis/cameraimpl.cpp
@@ -24,19 +24,11 @@ CameraObjects CameraImpl::getAllCameraObjects()
 bool CameraImpl::isGoalYellow()
 {
 	Mat goal;
-	int white = 0;
 	filterFrameAndConvertToHLS();
 	inRange(m_fileredFrame, cv::Scalar(20, 100, 50), cv::Scalar(30, 200, 255), goal);
 	Rect range(0, 130, 320, 110);
 	goal = goal(range);
-	for (int i = 0; i < range.height; ++i) {
-		for (int j = 0; j < range.width; j++)
-		{
-			if (goal.at<uchar>(i, j) == 255.0)
-				white++;
-		}
-	}
-	if(white > 0.7*range.area())
+	if(countNonZero(goal) > 0.7*range.area())
 		return true;
 	else
 		return false;
