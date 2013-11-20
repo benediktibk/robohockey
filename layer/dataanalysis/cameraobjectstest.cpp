e4852e282918b153df5f76caf802cc9310d2297b
Markus Hofbauer <markus.hofbauer@tum.de>
2013-11-20 13:49:02 +0100

    add test for function

--- layer/dataanalysis/cameraobjectstest.cpp
+++ layer/dataanalysis/cameraobjectstest.cpp
@@ -27,3 +27,33 @@ void CameraObjectsTest::getColorType_add2Objects_secondObjectIsYellow()
 
 	CPPUNIT_ASSERT_EQUAL(ColorTypeYellow, objects.back().getColorType());
 }
+
+void CameraObjectsTest::getCameraObjectAtPosition_4ObjectsAndPositionIs3And3_ObjectAtPositionIsObject3()
+{
+	CameraObjects objects;
+	CameraObject objectOne(ColorTypeUnknown, Point(0,0));
+	CameraObject objectTwo(ColorTypeYellow, Point(0,1));
+	CameraObject objectThree(ColorTypeBlue, Point(3.03,3.04));
+	CameraObject objectFour(ColorTypeGreen, Point(3.03,3.5));
+
+	objects.addObject(objectOne);
+	objects.addObject(objectTwo);
+	objects.addObject(objectThree);
+	objects.addObject(objectFour);
+
+	CPPUNIT_ASSERT_EQUAL(objectThree.getColorType(), objects.getCameraObjectAtPosition(Point(3,3)).getColorType());
+}
+
+void CameraObjectsTest::getCameraObjectAtPosition_3ObjectsAndPositionIs3And3AndNoObjectInRange_NextObjectAtPositionIsObject2()
+{
+	CameraObjects objects;
+	CameraObject objectOne(ColorTypeUnknown, Point(0,0));
+	CameraObject objectTwo(ColorTypeYellow, Point(0,1));
+	CameraObject objectThree(ColorTypeBlue, Point(3.5,3.5));
+
+	objects.addObject(objectOne);
+	objects.addObject(objectTwo);
+	objects.addObject(objectThree);
+
+	CPPUNIT_ASSERT_EQUAL(objectThree.getColorType(), objects.getCameraObjectAtPosition(Point(3,3)).getColorType());
+}
--- layer/dataanalysis/cameraobjectstest.h
+++ layer/dataanalysis/cameraobjectstest.h
@@ -16,11 +16,15 @@ namespace DataAnalysis
 		CPPUNIT_TEST_SUITE(CameraObjectsTest);
 		CPPUNIT_TEST(getObjectCount_add1Object_objectCountIs1);
 		CPPUNIT_TEST(getColorType_add2Objects_secondObjectIsYellow);
+		CPPUNIT_TEST(getCameraObjectAtPosition_4ObjectsAndPositionIs3And3_ObjectAtPositionIsObject3);
+		CPPUNIT_TEST(getCameraObjectAtPosition_3ObjectsAndPositionIs3And3AndNoObjectInRange_NextObjectAtPositionIsObject2);
 		CPPUNIT_TEST_SUITE_END();
 
 	private:
 		void getObjectCount_add1Object_objectCountIs1();
 		void getColorType_add2Objects_secondObjectIsYellow();
+		void getCameraObjectAtPosition_4ObjectsAndPositionIs3And3_ObjectAtPositionIsObject3();
+		void getCameraObjectAtPosition_3ObjectsAndPositionIs3And3AndNoObjectInRange_NextObjectAtPositionIsObject2();
 	};
 }
 }
