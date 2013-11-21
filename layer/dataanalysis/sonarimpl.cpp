12d32c28b2cc16425f7706e1ddd8e62d0ed27a71
Benedikt Schmidt <benediktibk@aon.at>
2013-11-21 11:49:04 +0100

    improved collision detection

--- layer/dataanalysis/sonarimpl.cpp
+++ layer/dataanalysis/sonarimpl.cpp
@@ -8,7 +8,7 @@ SonarImpl::SonarImpl(Hardware::Sonar &sonar, double minimumDistanceToObstacle, d
 	m_sonar(sonar),
 	m_distanceOfSensorsToMiddleOfRobot(0.15),
 	m_minimumDistanceToObstacle(minimumDistanceToObstacle - m_distanceOfSensorsToMiddleOfRobot),
-	m_minimumDistanceWithAngst(m_minimumDistanceToObstacle + 0.05),
+	m_minimumDistanceWithAngst(m_minimumDistanceToObstacle + 0.1),
 	m_timeToStop(timeToStop)
 { }
 
