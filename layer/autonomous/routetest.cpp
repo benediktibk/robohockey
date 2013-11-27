af238ea4d14b13e1b46dbf250829e917ad25e241
Benedikt Schmidt <benediktibk@aon.at>
2013-11-27 12:45:35 +0100

    added tests for Route

--- layer/autonomous/routetest.cpp
+++ layer/autonomous/routetest.cpp
@@ -3,3 +3,45 @@
 
 using namespace RoboHockey::Common;
 using namespace RoboHockey::Layer::Autonomous;
+
+void RouteTest::constructor_empty_isNotValid()
+{
+	Route route;
+
+	CPPUNIT_ASSERT(!route.isValid());
+}
+
+void RouteTest::constructor_empty_pointCountIs0()
+{
+	Route route;
+
+	CPPUNIT_ASSERT_EQUAL((size_t)0, route.getPointCount());
+}
+
+void RouteTest::addPoint_onceCalled_pointCountIs1()
+{
+	Route route;
+
+	route.addPoint(Point(1, 2));
+
+	CPPUNIT_ASSERT_EQUAL((size_t)1, route.getPointCount());
+}
+
+void RouteTest::addPoint_twiceCalled_pointCountIs2()
+{
+	Route route;
+
+	route.addPoint(Point(1, 2));
+	route.addPoint(Point(3, 2));
+
+	CPPUNIT_ASSERT_EQUAL((size_t)2, route.getPointCount());
+}
+
+void RouteTest::isValid_onePointAdded_true()
+{
+	Route route;
+
+	route.addPoint(Point(1, 2));
+
+	CPPUNIT_ASSERT(route.isValid());
+}
--- layer/autonomous/routetest.h
+++ layer/autonomous/routetest.h
@@ -14,9 +14,19 @@ namespace Autonomous
 			public CPPUNIT_NS::TestFixture
 	{
 		CPPUNIT_TEST_SUITE(RouteTest);
+		CPPUNIT_TEST(constructor_empty_isNotValid);
+		CPPUNIT_TEST(constructor_empty_pointCountIs0);
+		CPPUNIT_TEST(addPoint_onceCalled_pointCountIs1);
+		CPPUNIT_TEST(addPoint_twiceCalled_pointCountIs2);
+		CPPUNIT_TEST(isValid_onePointAdded_true);
 		CPPUNIT_TEST_SUITE_END();
 
 	private:
+		void constructor_empty_isNotValid();
+		void constructor_empty_pointCountIs0();
+		void addPoint_onceCalled_pointCountIs1();
+		void addPoint_twiceCalled_pointCountIs2();
+		void isValid_onePointAdded_true();
 	};
 }
 }
