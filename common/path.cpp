7986fcfe943f1ec65ebee9d6a6fbaf59fa18f42a
Markus Hofbauer <markus.hofbauer@tum.de>
2013-11-28 15:10:05 +0100

    fixed path and add width to start and end

--- common/path.cpp
+++ common/path.cpp
@@ -2,6 +2,7 @@
 #include "common/circle.h"
 #include "common/line.h"
 #include "common/angle.h"
+#include <math.h>
 
 using namespace RoboHockey::Common;
 
@@ -15,10 +16,10 @@ bool Path::intersectsWith(const Circle &circle) const
 {
 	Angle angleBetweenPoints(m_start, m_end);
 	Point circleCenter = circle.getCenter();
-	Point startLeft(m_start + Point(0.5*m_width, Angle::getQuarterRotation() + angleBetweenPoints));
-	Point startRight(m_start + Point(0.5*m_width, Angle::getQuarterRotation() - angleBetweenPoints));
-	Point endLeft(m_end + Point(0.5*m_width, Angle::getQuarterRotation() + angleBetweenPoints));
-	Point endRight(m_end + Point(0.5*m_width, Angle::getQuarterRotation() - angleBetweenPoints));
+	Point startLeft(m_start + Point(sqrt(2)*0.5*m_width, Angle::getQuarterRotation() + Angle::getEighthRotation() + angleBetweenPoints));
+	Point startRight(m_start + Point(sqrt(2)*0.5*m_width, angleBetweenPoints - Angle::getQuarterRotation() - Angle::getEighthRotation()));
+	Point endLeft(m_end + Point(sqrt(2)*0.5*m_width, Angle::getEighthRotation() + angleBetweenPoints));
+	Point endRight(m_end + Point(sqrt(2)*0.5*m_width, angleBetweenPoints - Angle::getEighthRotation()));
 	Line leftOutline(startLeft, endLeft);
 	Line rightOutline(startRight, endRight);
 	Line startOutline(startLeft, startRight);
