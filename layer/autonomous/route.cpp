e6ba37cce38c87981f69e3524d66bfe568fada52
Markus Hofbauer <markus.hofbauer@tum.de>
2013-11-28 17:20:43 +0100

    implemented route::intersectsWith

--- layer/autonomous/route.cpp
+++ layer/autonomous/route.cpp
@@ -1,4 +1,5 @@
 #include "layer/autonomous/route.h"
+#include "common/path.h"
 #include <assert.h>
 
 using namespace std;
@@ -49,7 +50,16 @@ double Route::getWidth() const
 	return m_width;
 }
 
-bool Route::intersectsWith(const std::vector<Common::Circle> &/*objects*/) const
+bool Route::intersectsWith(const vector<Circle> &objects) const
 {
+	for(list<Point>::const_iterator k = m_points.begin(); k != m_points.end(); ++k)
+	{
+		Path currentPath(*k,*k++, m_width);
+		for(vector<Circle>::const_iterator i = objects.begin(); i != objects.end(); ++i)
+		{
+			if(currentPath.intersectsWith(*i))
+				return true;
+		}
+	}
 	return false;
 }
