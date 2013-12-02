34463b77aef7395a563cd8ec48a5f40513858b0e
Benedikt Schmidt <benediktibk@aon.at>
2013-12-02 19:18:57 +0100

    fixed sonar distance for collision detection when there is a puck ahead

--- layer/dataanalysis/sonarimpl.cpp
+++ layer/dataanalysis/sonarimpl.cpp
@@ -8,18 +8,13 @@ SonarImpl::SonarImpl(Hardware::Sonar &sonar) :
 	m_sonar(sonar)
 { }
 
-bool SonarImpl::isObstacleDirectInFront(double speed)
+bool SonarImpl::isObstacleDirectInFront(double)
 {
 	const double leftFrontValue = m_sonar.getDistanceForSensor(3);
 	const double rightFrontValue = m_sonar.getDistanceForSensor(4);
-	const double distanceOfSensorsToMiddleOfRobot = 0.15;
-	const double minimumDistanceToObstacle = 0.8 - distanceOfSensorsToMiddleOfRobot;
-	const double timeToStop = 0.2;
-	const double brakingDistance = speed*timeToStop;
-	const double totalDistance = brakingDistance + minimumDistanceToObstacle;
 
-	if (	leftFrontValue < totalDistance ||
-			rightFrontValue < totalDistance)
+	if (	leftFrontValue < 0.2 ||
+			rightFrontValue < 0.2)
 		return true;
 	else
 		return false;
