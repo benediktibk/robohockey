053fa6413ba0ca36e7476641dabfe6f6b72ad694
Markus Hofbauer <markus.hofbauer@tum.de>
2013-12-03 14:48:08 +0100

    first version on getpointsbesideobstacle

--- layer/autonomous/router.cpp
+++ layer/autonomous/router.cpp
@@ -1,6 +1,9 @@
 #include "layer/autonomous/router.h"
 #include "layer/autonomous/field.h"
 #include "layer/autonomous/route.h"
+#include "common/path.h"
+#include "common/pathintersectpoints.h"
+#include "common/angle.h"
 
 using namespace std;
 using namespace RoboHockey::Common;
@@ -23,7 +26,20 @@ Route Router::calculateRoute(const Point &/*start*/, const Point &end) const
 		return route;
 }
 
-vector<Point> Router::getPointsBesideObstacle(const Path &/*path*/, const Circle &/*obstacle*/) const
+vector<Point> Router::getPointsBesideObstacle(const Path &path, const Circle &obstacle) const
 {
+	PathIntersectPoints intersectionPoints = path.getIntersectPoints(obstacle);
+	Angle angleBetweenCircleCenterAndIntersectPoint(obstacle.getCenter(), intersectionPoints.getIntersectPoints().front());
+	Point pointOnObstacleEdge;
+
+	if(intersectionPoints.getIntersectPointsCount() == 1)
+	{
+		pointOnObstacleEdge = intersectionPoints.getIntersectPoints().front();
+	}
+	else
+	{
+
+	}
+
 	return vector<Point>();
 }
