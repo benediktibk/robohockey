7b5b42b84731d4981a8d2403e7cbf259a2a4897c
Markus Hofbauer <markus.hofbauer@tum.de>
2013-12-04 12:18:55 +0100

    add more tests

--- layer/autonomous/routertest.cpp
+++ layer/autonomous/routertest.cpp
@@ -38,7 +38,7 @@ void RouterTest::calculateRoute_emptyField_routeHasSameWidthAsRobot()
 	CPPUNIT_ASSERT_DOUBLES_EQUAL(0.5, route.getWidth(), 0.00001);
 }
 
-void RouterTest::getPointsBesideObstacle_intersectFromLeft_shortPointIs()
+void RouterTest::getPointsBesideObstacle_intersectFromLeftAndCircleCenterNotOnPath_shortPointIs2AndMinus0p25()
 {
 	Compare compare(0.0001);
 	FieldMock field;
@@ -47,4 +47,37 @@ void RouterTest::getPointsBesideObstacle_intersectFromLeft_shortPointIs()
 	Circle obstacle(Point(2,2), 2.8284);
 
 	CPPUNIT_ASSERT(compare.isFuzzyEqual(Point(2,-0.25), router.getPointsBesideObstacle(currentPath, obstacle).front()));
+}
+
+void RouterTest::getPointsBesideObstacle_intersectFromRightAndCircleCenterNotOnPath_shortPointIs2And2p25()
+{
+	Compare compare(0.0001);
+	FieldMock field;
+	Router router(0.5, field);
+	Path currentPath(Point(0,2), Point(4,2), 2);
+	Circle obstacle(Point(2,0), 2.8284);
+
+	CPPUNIT_ASSERT(compare.isFuzzyEqual(Point(2,2.25), router.getPointsBesideObstacle(currentPath, obstacle).front()));
+}
+
+void RouterTest::getPointsBesideObstacle_intersectFromLeftAndCircleCenterNotOnPath_shortPointIs1p1767And0p8232()
+{
+	Compare compare(0.0001);
+	FieldMock field;
+	Router router(0.5, field);
+	Path currentPath(Point(0,0), Point(3,3), 1.4142);
+	Circle obstacle(Point(0,2), 2);
+
+	CPPUNIT_ASSERT(compare.isFuzzyEqual(Point(1.1767,0.8232), router.getPointsBesideObstacle(currentPath, obstacle).front()));
+}
+
+void RouterTest::getPointsBesideObstacle_intersectFromRightAndCircleCenterNotOnPath_shortPointIs0p8232And1p1767()
+{
+	Compare compare(0.0001);
+	FieldMock field;
+	Router router(0.5, field);
+	Path currentPath(Point(0,0), Point(3,3), 1.4142);
+	Circle obstacle(Point(2,0), 2);
+
+	CPPUNIT_ASSERT(compare.isFuzzyEqual(Point(0.8232,1.1767), router.getPointsBesideObstacle(currentPath, obstacle).front()));
 }
--- layer/autonomous/routertest.h
+++ layer/autonomous/routertest.h
@@ -17,14 +17,20 @@ namespace Autonomous
 		CPPUNIT_TEST(calculateRoute_emptyField_validRoute);
 		CPPUNIT_TEST(calculateRoute_emptyField_onlyPointOfRouteIsTarget);
 		CPPUNIT_TEST(calculateRoute_emptyField_routeHasSameWidthAsRobot);
-		CPPUNIT_TEST(getPointsBesideObstacle_intersectFromLeft_shortPointIs);
+		CPPUNIT_TEST(getPointsBesideObstacle_intersectFromLeftAndCircleCenterNotOnPath_shortPointIs2AndMinus0p25);
+		CPPUNIT_TEST(getPointsBesideObstacle_intersectFromRightAndCircleCenterNotOnPath_shortPointIs2And2p25);
+		CPPUNIT_TEST(getPointsBesideObstacle_intersectFromLeftAndCircleCenterNotOnPath_shortPointIs1p1767And0p8232);
+		CPPUNIT_TEST(getPointsBesideObstacle_intersectFromRightAndCircleCenterNotOnPath_shortPointIs0p8232And1p1767);
 		CPPUNIT_TEST_SUITE_END();
 
 	private:
 		void calculateRoute_emptyField_validRoute();
 		void calculateRoute_emptyField_onlyPointOfRouteIsTarget();
 		void calculateRoute_emptyField_routeHasSameWidthAsRobot();
-		void getPointsBesideObstacle_intersectFromLeft_shortPointIs();
+		void getPointsBesideObstacle_intersectFromLeftAndCircleCenterNotOnPath_shortPointIs2AndMinus0p25();
+		void getPointsBesideObstacle_intersectFromRightAndCircleCenterNotOnPath_shortPointIs2And2p25();
+		void getPointsBesideObstacle_intersectFromLeftAndCircleCenterNotOnPath_shortPointIs1p1767And0p8232();
+		void getPointsBesideObstacle_intersectFromRightAndCircleCenterNotOnPath_shortPointIs0p8232And1p1767();
 	};
 }
 }
