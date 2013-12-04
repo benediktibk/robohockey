dbd5b1d5d209b8f0b0f625bbfda2a783a0f69d26
Markus Hofbauer <markus.hofbauer@tum.de>
2013-12-04 18:42:23 +0100

    removed solved todos

--- layer/autonomous/router.cpp
+++ layer/autonomous/router.cpp
@@ -43,7 +43,6 @@ vector<Point> Router::getPointsBesideObstacle(const Path &path, const Circle &ob
 	}
 	else
 	{
-		//! this offset is probably wrong
 		offsetDistance = 0.5*m_robotWidth + shortPointBesideObstacle.distanceTo(intersectionPoints.front())
 						+ 2*shortPointBesideObstacle.distanceTo(obstacle.getCenter());
 	}
