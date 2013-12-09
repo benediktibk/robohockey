641c6c5aca0b55ef47362d1cb05bac909cb5296c
Florian Spinnler <a25@o2mail.de>
2013-12-09 23:52:32 +0100

    changed achieveGoals

--- layer/strategy/achievegoalstest.cpp
+++ layer/strategy/achievegoalstest.cpp
@@ -21,9 +21,43 @@ void AchieveGoalsTest::nextState_stopMovement_pause()
     referee.setDetectionStart(false);
     field.setAchievedGoals(0);
     referee.setGameStart(false);
+    referee.setGameOver(false);
+    State *state;
+    state = achieveGoals.nextState();
+    Pause *stateCasted = dynamic_cast<Pause*>(state);
+    CPPUNIT_ASSERT(stateCasted != 0);
+}
+
+void AchieveGoalsTest::nextState_gameOver_pause()
+{
+    RobotMock robot;
+    FieldMock field;
+    RefereeMock referee;
+    AchieveGoals achieveGoals(robot, field, referee);
+    referee.setStopMovement(false);
+    referee.setDetectionStart(false);
+    field.setAchievedGoals(0);
+    referee.setGameStart(false);
     referee.setGameOver(true);
     State *state;
     state = achieveGoals.nextState();
     Pause *stateCasted = dynamic_cast<Pause*>(state);
+    CPPUNIT_ASSERT(stateCasted != 0);
+}
+
+void AchieveGoalsTest::nextState_achievedGoals3_hideEnemyPucks()
+{
+    RobotMock robot;
+    FieldMock field;
+    RefereeMock referee;
+    AchieveGoals achieveGoals(robot, field, referee);
+    referee.setStopMovement(false);
+    referee.setDetectionStart(false);
+    field.setAchievedGoals(3);
+    referee.setGameStart(false);
+    referee.setGameOver(false);
+    State *state;
+    state = achieveGoals.nextState();
+    HideEnemyPucks *stateCasted = dynamic_cast<HideEnemyPucks*>(state);
     CPPUNIT_ASSERT(stateCasted != 0);
 }
--- layer/strategy/achievegoalstest.h
+++ layer/strategy/achievegoalstest.h
@@ -15,10 +15,14 @@ class AchieveGoalsTest:
     {
         CPPUNIT_TEST_SUITE(AchieveGoalsTest);
         CPPUNIT_TEST(nextState_stopMovement_pause);
+        CPPUNIT_TEST(nextState_gameOver_pause);
+        CPPUNIT_TEST(nextState_achievedGoals3_hideEnemyPucks);
         CPPUNIT_TEST_SUITE_END();
 
     private:
         void nextState_stopMovement_pause();
+        void nextState_gameOver_pause();
+        void nextState_achievedGoals3_hideEnemyPucks();
 
 
 
@@ -31,33 +35,3 @@ class AchieveGoalsTest:
 
 #endif
 
-
-#ifndef ROBOHOCKEY_LAYER_STRATEGY_INITIALSTATETEST_H
-#define ROBOHOCKEY_LAYER_STRATEGY_INITIALSTATETEST_H
-
-#include <cppunit/TestFixture.h>
-#include <cppunit/extensions/HelperMacros.h>
-
-namespace RoboHockey
-{
-namespace Layer
-{
-namespace Strategy
-{
-    class InitialStateTest :
-            public CPPUNIT_NS::TestFixture
-    {
-        CPPUNIT_TEST_SUITE(InitialStateTest);
-        CPPUNIT_TEST(nextState_detectionStart_calibrate);
-        CPPUNIT_TEST(nextState_gameStartAndDetectionStart_calibrate);
-        CPPUNIT_TEST_SUITE_END();
-
-    private:
-        void nextState_detectionStart_calibrate();
-        void nextState_gameStartAndDetectionStart_calibrate();
-    };
-}
-}
-}
-
-#endif
