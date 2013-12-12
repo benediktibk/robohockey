77cf950ee7825f6663c5f629797d129b7fc80312
Kevin Meyer <meyer.kevin@mytum.de>
2013-12-12 16:41:52 +0100

    changed and renamed tests.

--- layer/strategy/fielddetectionstatemachine/checkgoalcolortest.h
+++ layer/strategy/fielddetectionstatemachine/checkgoalcolortest.h
@@ -16,13 +16,13 @@ class CheckGoalColorTest :
 		public CPPUNIT_NS::TestFixture
 	{
 		CPPUNIT_TEST_SUITE(CheckGoalColorTest);
-		CPPUNIT_TEST(nextState_afterColorCheck_driveToWaitingPosition);
-		CPPUNIT_TEST(nextState_noColorCheck_NULL);
+		CPPUNIT_TEST(nextState_successfulColorCheck_driveToWaitingPosition);
+		CPPUNIT_TEST(nextState_unsuccessfulColorCheck_NULL);
 		CPPUNIT_TEST_SUITE_END();
 
 	private:
-		void nextState_afterColorCheck_driveToWaitingPosition();
-		void nextState_noColorCheck_NULL();
+		void nextState_successfulColorCheck_driveToWaitingPosition();
+		void nextState_unsuccessfulColorCheck_NULL();
 	};
 }
 }
