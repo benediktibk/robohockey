7fc1dcb40f69f12e404fb2d23529eb7840decda8
Kevin Meyer <meyer.kevin@mytum.de>
2013-12-12 12:37:45 +0100

    added test for CheckGoalColor

--- layer/strategy/fielddetectionstatemachine/checkgoalcolortest.cpp
+++ layer/strategy/fielddetectionstatemachine/checkgoalcolortest.cpp
@@ -3,6 +3,7 @@
 #include "layer/strategy/common/statemachine.h"
 #include "layer/strategy/common/statemock.h"
 #include "layer/strategy/common/refereemock.h"
+#include "layer/strategy/common/driveto.h"
 #include "layer/autonomous/robotmock.h"
 #include "layer/autonomous/fieldmock.h"
 
@@ -10,6 +11,22 @@ using namespace RoboHockey::Layer::Strategy::Common;
 using namespace RoboHockey::Layer::Strategy::FieldDetectionStateMachine;
 using namespace RoboHockey::Layer::Autonomous;
 
+
+void CheckGoalColorTest::nextState_afterColorCheck_driveToWaitingPosition()
+{
+	RobotMock robot;
+	FieldMock field;
+	RefereeMock referee;
+	CheckGoalColor checkGoalColorState(robot, field, referee);
+
+	checkGoalColorState.update();
+
+	State *state;
+	state = checkGoalColorState.nextState();
+	DriveTo *stateCasted = dynamic_cast<DriveTo*>(state);
+	CPPUNIT_ASSERT(stateCasted != 0);
+
+}
 
 
 
--- layer/strategy/fielddetectionstatemachine/checkgoalcolortest.h
+++ layer/strategy/fielddetectionstatemachine/checkgoalcolortest.h
@@ -16,9 +16,11 @@ class CheckGoalColorTest :
 		public CPPUNIT_NS::TestFixture
 	{
 		CPPUNIT_TEST_SUITE(CheckGoalColorTest);
+		CPPUNIT_TEST(nextState_afterColorCheck_driveToWaitingPosition);
 		CPPUNIT_TEST_SUITE_END();
+
 	private:
-		
+		void nextState_afterColorCheck_driveToWaitingPosition();
 	};
 }
 }
