9b2d0c5c93bb6b0b0bb8a2818456cc8b6f3d6f7c
Markus Hofbauer <markus.hofbauer@tum.de>
2013-12-17 16:47:44 +0100

    hide pucks starts drivepuckstatemachine

--- layer/strategy/mainstatemachine/hideenemypucks.cpp
+++ layer/strategy/mainstatemachine/hideenemypucks.cpp
@@ -2,13 +2,25 @@
 #include "layer/strategy/mainstatemachine/drivetofinalposition.h"
 #include "layer/strategy/common/referee.h"
 #include "layer/autonomous/field.h"
+#include "layer/strategy/drivepuckstatemachine/initialstate.h"
+#include "layer/strategy/mainstatemachine/pause.h"
 
 using namespace RoboHockey::Layer::Strategy::Common;
 using namespace RoboHockey::Layer::Strategy::MainStateMachine;
+using namespace RoboHockey::Layer::Autonomous;
 
 HideEnemyPucks::HideEnemyPucks(Autonomous::Robot &robot, Autonomous::Field &field, Referee &referee):
     State(robot, field, referee)
-{ }
+{
+	State *initialState = new DrivePuckStateMachine::InitialState(robot, field, referee);
+	m_drivePuckStateMachine = new StateMachine(initialState, robot, field, referee);
+}
+
+HideEnemyPucks::~HideEnemyPucks()
+{
+	delete m_drivePuckStateMachine;
+	m_drivePuckStateMachine = 0;
+}
 
 State* HideEnemyPucks::nextState()
 {
@@ -23,5 +35,7 @@ State* HideEnemyPucks::nextState()
 }
 
 void HideEnemyPucks::update()
-{ }
+{
+	m_drivePuckStateMachine->update();
+}
 
