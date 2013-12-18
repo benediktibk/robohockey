2052702527281243d0645c6530ebace151b34fae
Benedikt Schmidt <benediktibk@aon.at>
2013-12-18 18:31:51 +0100

    collisions with the sonar are only detected if both sensors in the front register one

--- layer/dataanalysis/sonarimpl.cpp
+++ layer/dataanalysis/sonarimpl.cpp
@@ -12,7 +12,6 @@ bool SonarImpl::isObstacleDirectInFront(double speed)
 {
 	const double leftFrontValue = m_sonar.getDistanceForSensor(3);
 	const double rightFrontValue = m_sonar.getDistanceForSensor(4);
-	const double meanValue = (leftFrontValue + rightFrontValue)/2;
 	const double distanceOfSensorsToMiddleOfRobot = 0.15;
 	const double minimumDistanceToObstacle = 0.8 - distanceOfSensorsToMiddleOfRobot;
 	const double timeToStop = 0.2;
@@ -20,12 +19,8 @@ bool SonarImpl::isObstacleDirectInFront(double speed)
 	const double totalDistance = brakingDistance + minimumDistanceToObstacle;
 	const double crapBorderForSonar = 0.2;
 
-	if (leftFrontValue > crapBorderForSonar && rightFrontValue > crapBorderForSonar)
-		return meanValue < totalDistance;
-	else if (leftFrontValue > crapBorderForSonar)
-		return leftFrontValue < totalDistance;
-	else if (rightFrontValue > crapBorderForSonar)
-		return rightFrontValue < totalDistance;
-	else
+	if (leftFrontValue < crapBorderForSonar || rightFrontValue < crapBorderForSonar)
 		return false;
+	else
+		return	leftFrontValue < totalDistance && rightFrontValue < totalDistance;
 }
