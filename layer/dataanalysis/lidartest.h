26adba64fc66589173e46fb111a8a42588017442
Kevin Meyer <meyer.kevin@mytum.de>
2013-12-18 13:42:58 +0100

    added test for lidarimpl. CanBeSeen returns wrong result, when robot is in position != (0,0)

--- layer/dataanalysis/lidartest.h
+++ layer/dataanalysis/lidartest.h
@@ -96,6 +96,7 @@ namespace DataAnalysis
 		CPPUNIT_TEST(canBeSeen_objectRightAndLookingLeft_false);
 		CPPUNIT_TEST(canBeSeen_objectRightLowerAndLookingLeft_false);
 		CPPUNIT_TEST(canBeSeen_objectDirectBehindOtherObstacle_true);
+		CPPUNIT_TEST(canBeSeen_robotNotInOriginObjectVisible_true);
 		CPPUNIT_TEST_SUITE_END();
 
 	private:
@@ -174,6 +175,7 @@ namespace DataAnalysis
 		void canBeSeen_objectRightAndLookingLeft_false();
 		void canBeSeen_objectRightLowerAndLookingLeft_false();
 		void canBeSeen_objectDirectBehindOtherObstacle_true();
+		void canBeSeen_robotNotInOriginObjectVisible_true();
 
 	private:
 		static const double m_maximumDistance;
