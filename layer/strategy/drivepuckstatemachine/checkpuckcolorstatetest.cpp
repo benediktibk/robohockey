d776a939b69c99ba89a9e0e9fd92975614ad7484
Markus Hofbauer <markus.hofbauer@tum.de>
2013-12-19 16:46:35 +0100

    renamed class

--- layer/strategy/drivepuckstatemachine/checkpuckcolorstatetest.cpp
+++
@@ -1,7 +0,0 @@
-#include "layer/strategy/drivepuckstatemachine/checkpuckcolorstatetest.h"
-#include "layer/strategy/drivepuckstatemachine/checkpuckcolorstate.h"
-
-using namespace RoboHockey::Layer::Strategy::DrivePuckStateMachine;
-
-
-
