bbfb17648577bdabf57729d16d741fa2bb095998
Markus Hofbauer <markus.hofbauer@tum.de>
2013-12-20 17:18:14 +0100

    chaned findpuck

--- layer/strategy/drivepuckstatemachine/findpuckstate.cpp
+++ layer/strategy/drivepuckstatemachine/findpuckstate.cpp
@@ -1,23 +1,32 @@
 #include "layer/strategy/drivepuckstatemachine/findpuckstate.h"
-#include "layer/strategy/drivepuckstatemachine/collectpuckstate.h"
+#include "layer/strategy/drivepuckstatemachine/drivetocollectpuckstate.h"
+#include "layer/strategy/common/driveto.h"
 #include "layer/strategy/common/referee.h"
 #include "layer/autonomous/robot.h"
 #include "layer/autonomous/field.h"
-#include "common/robotposition.h"
 
 using namespace RoboHockey::Layer::Strategy::Common;
 using namespace RoboHockey::Layer::Strategy::DrivePuckStateMachine;
 using namespace RoboHockey::Layer::Autonomous;
 
-FindPuckState::FindPuckState(Robot &robot, Field &field, Referee &referee, DrivePuck *drivePuck) :
-	DriveTo(robot, field, referee, field.getTargetsForSearchingPucks(),
-			new CollectPuckState(robot, field, referee, drivePuck),
-			0)
+FindPuckState::FindPuckState(Robot &robot, Field &field, Referee &referee, DrivePuck *drivePuck):
+	State(robot, field, referee, true),
+	m_drivePuck(drivePuck)
+{ }
+
+State *FindPuckState::nextState()
 {
-	//! @todo Should handle a "can't reach Target".
+	return new DriveTo(m_robot, m_field, m_referee, m_field.getTargetsForSearchingPucks(),
+					   new DriveToCollectPuckState(m_robot, m_field, m_referee, m_drivePuck),
+					   new FindPuckState(m_robot, m_field, m_referee, m_drivePuck));
+
 }
 
 std::string FindPuckState::getName()
 {
 	return "FindPuck";
+}
+
+void FindPuckState::updateInternal()
+{
 }
