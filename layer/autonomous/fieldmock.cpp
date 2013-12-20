4277bbc64dd90ff7804f5d13d5f17dd0a2c2bd6a
Kevin Meyer <meyer.kevin@mytum.de>
2013-12-20 13:21:18 +0100

    changes in FieldMock

--- layer/autonomous/fieldmock.cpp
+++ layer/autonomous/fieldmock.cpp
@@ -77,6 +77,11 @@ void FieldMock::setCalibrationReturn(bool value)
 	m_calibrated = value;
 }
 
+void FieldMock::setCalibrationReturnPosition(RobotPosition &newOrigin)
+{
+	m_newOrigin = newOrigin;
+}
+
 void FieldMock::setTrueTeamColor(FieldColor teamColor)
 {
 	m_teamColor = teamColor;
@@ -84,7 +89,7 @@ void FieldMock::setTrueTeamColor(FieldColor teamColor)
 
 RobotPosition FieldMock::getNewOriginFromFieldDetection()
 {
-	return RobotPosition();
+	return m_newOrigin;
 }
 
 void FieldMock::transformFieldToNewOrigin(const RobotPosition /*newOrigin*/)
