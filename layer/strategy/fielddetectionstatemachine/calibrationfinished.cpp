e7396ae4d3ed5fb3e4d927b2f09efa3127d21a20
Kevin Meyer <meyer.kevin@mytum.de>
2013-12-23 14:00:57 +0100

    Driving to Different Positions in CalibrationFinished State.

--- layer/strategy/fielddetectionstatemachine/calibrationfinished.cpp
+++ layer/strategy/fielddetectionstatemachine/calibrationfinished.cpp
@@ -1,18 +1,32 @@
 #include "layer/strategy/fielddetectionstatemachine/calibrationfinished.h"
 #include "layer/strategy/common/referee.h"
+#include "layer/strategy/common/driveto.h"
 #include "layer/autonomous/robot.h"
 #include "layer/autonomous/field.h"
+#include "common/robotposition.h"
 
+using namespace RoboHockey::Common;
 using namespace RoboHockey::Layer::Strategy::Common;
 using namespace RoboHockey::Layer::Strategy::FieldDetectionStateMachine;
 
-CalibrationFinished::CalibrationFinished(Autonomous::Robot &robot, Autonomous::Field &field, Common::Referee &referee) :
-	State(robot, field, referee, false)
+CalibrationFinished::CalibrationFinished(Autonomous::Robot &robot, Autonomous::Field &field, Common::Referee &referee, unsigned int reachedTargets) :
+	State(robot, field, referee, true),
+	m_reachedTargets(reachedTargets)
 { }
 
 State* CalibrationFinished::nextState()
 {
-	return 0;
+
+	if (m_reachedTargets <= 3)
+		return 0;
+
+	std::list<RobotPosition> targetList;
+	targetList.push_back(m_field.getTargetsForWaitingPhase()[m_reachedTargets]);
+
+	return new DriveTo(m_robot, m_field, m_referee,
+					   targetList,
+					   new CalibrationFinished(m_robot, m_field, m_referee, m_reachedTargets +1),
+					   new CalibrationFinished(m_robot, m_field, m_referee, m_reachedTargets +1));
 }
 
 std::string CalibrationFinished::getName()
