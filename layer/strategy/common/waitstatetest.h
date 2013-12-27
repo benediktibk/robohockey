6c7eb8d4e93c31d6411ff72949d270ab1c592cd2
Markus Hofbauer <markus.hofbauer@tum.de>
2013-12-27 10:12:35 +0100

    add tests for waitstate

--- layer/strategy/common/waitstatetest.h
+++ layer/strategy/common/waitstatetest.h
@@ -16,9 +16,13 @@ namespace Common
 			public CPPUNIT_NS::TestFixture
 	{
 		CPPUNIT_TEST_SUITE(WaitStateTest);
+		CPPUNIT_TEST(nextState_didntWaitEnoughCycles_resultIs0);
+		CPPUNIT_TEST(nextState_didWaitEnoughCycles_resultIsStateAfterWaitCycles);
 		CPPUNIT_TEST_SUITE_END();
 
 	private:
+		void nextState_didntWaitEnoughCycles_resultIs0();
+		void nextState_didWaitEnoughCycles_resultIsStateAfterWaitCycles();
 	
 };
 }
