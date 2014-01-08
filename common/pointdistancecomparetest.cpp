cfcbd5c0fb33e848e2a8f1d108d05c759d23c18b
Kevin Meyer <meyer.kevin@mytum.de>
2014-01-08 11:51:21 +0100

    added tests to PointDistanceCompare

--- common/pointdistancecomparetest.cpp
+++ common/pointdistancecomparetest.cpp
@@ -3,5 +3,20 @@
 
 using namespace RoboHockey::Common;
 
+void PointDistanceCompareTest::compare_firstOneCloserThanSecondOneBecauseOfPosition_true()
+{
+	Point one(3, 4);
+	Point two(-3, 4);
+	PointDistanceCompare compare(Point(2, 4));
 
+	CPPUNIT_ASSERT(compare(one, two));
+}
 
+void PointDistanceCompareTest::compare_seoncdOneCloserThanFirstOneBecauseOfPosition_false()
+{
+	Point one(3, 4);
+	Point two(-3, 4);
+	PointDistanceCompare compare(Point(-3, 3));
+
+	CPPUNIT_ASSERT(!compare(one, two));
+}
--- common/pointdistancecomparetest.h
+++ common/pointdistancecomparetest.h
@@ -12,9 +12,14 @@ class PointDistanceCompareTest :
 		public CPPUNIT_NS::TestFixture
 	{
 		CPPUNIT_TEST_SUITE(PointDistanceCompareTest);
+		CPPUNIT_TEST(compare_firstOneCloserThanSecondOneBecauseOfPosition_true);
+		CPPUNIT_TEST(compare_seoncdOneCloserThanFirstOneBecauseOfPosition_false);
 		CPPUNIT_TEST_SUITE_END();
+
 	private:
-		
+
+		void compare_firstOneCloserThanSecondOneBecauseOfPosition_true();
+		void compare_seoncdOneCloserThanFirstOneBecauseOfPosition_false();
 	};
 }
 }
