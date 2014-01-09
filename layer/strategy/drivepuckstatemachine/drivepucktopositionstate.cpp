57052939f838c41afebe36c4d2ab61117ce825f4
Kevin Meyer <meyer.kevin@mytum.de>
2014-01-09 18:04:03 +0100

    increase goal count in leavepuckstate

--- layer/strategy/drivepuckstatemachine/drivepucktopositionstate.cpp
+++ layer/strategy/drivepuckstatemachine/drivepucktopositionstate.cpp
@@ -3,6 +3,7 @@
 #include "layer/strategy/drivepuckstatemachine/drivetocollectpuckstate.h"
 #include "layer/strategy/common/drivetostate.h"
 #include "layer/autonomous/robot.h"
+#include "layer/strategy/common/colordependentpucktargetfetcher.h"
 
 using namespace RoboHockey::Layer::Strategy::Common;
 using namespace RoboHockey::Layer::Strategy::DrivePuckStateMachine;
@@ -18,7 +19,7 @@ State *DrivePuckToPositionState::nextState()
 	if(m_robot.isPuckCollected())
 		return new DriveToState(
 					m_robot, m_field, m_referee, m_puckTargetFetcher.getTargetPositions(),
-					new LeavePuckState(m_robot, m_field, m_referee, m_puckTargetFetcher),
+					new LeavePuckState(m_robot, m_field, m_referee, m_puckTargetFetcher, m_puckTargetFetcher.isAchievingGoals()),
 					new LeavePuckState(m_robot, m_field, m_referee, m_puckTargetFetcher));
 	else
 		return new DriveToCollectPuckState(m_robot, m_field, m_referee, m_puckTargetFetcher);
