5260a5b0e3b0406e4de0d112767c581d51cdad61
Benedikt Schmidt <benediktibk@aon.at>
2014-01-10 11:08:22 +0100

    improved smooth turns

--- layer/dataanalysis/engineimpl.cpp
+++ layer/dataanalysis/engineimpl.cpp
@@ -237,7 +237,7 @@ void EngineImpl::driveAndTurn(const RobotPosition &currentPosition)
 	double rotationSpeed = orientationAmplification*orthogonalError;
 	double magnitude = calculateSpeedForGoingStraight(forwardError) + m_finalSpeed;
 
-	double magnitudeModification = 1 - fabs(rotationSpeed);
+	double magnitudeModification = 1 - fabs(rotationSpeed*2);
 	if (magnitudeModification > 1)
 		magnitudeModification = 1;
 	magnitude *= magnitudeModification;
