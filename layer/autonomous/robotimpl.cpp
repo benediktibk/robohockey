61f02ba053b7db5a8e00bf7924daff46ef8ec010
Benedikt Schmidt <benediktibk@aon.at>
2014-01-10 11:23:17 +0100

    improved smooth turns

--- layer/autonomous/robotimpl.cpp
+++ layer/autonomous/robotimpl.cpp
@@ -548,7 +548,7 @@ double RobotImpl::calculateFinalSpeedForGoingStraight(
 {
 	Angle angle = Angle::getHalfRotation() - Angle(next, current, nextButOne);
 	angle.abs();
-	const Angle maximumAngle = Angle::getQuarterRotation();
+	const Angle maximumAngle = Angle::getHalfRotation()/3;
 
 	if (angle.getValueBetweenZeroAndTwoPi() > maximumAngle.getValueBetweenZeroAndTwoPi())
 		return 0;
