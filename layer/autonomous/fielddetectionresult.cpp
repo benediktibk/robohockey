73d9b61fa0972a64e9c1bbd69a3a09aa6793f817
Kevin Meyer <meyer.kevin@mytum.de>
2014-01-16 13:25:08 +0100

    implementing FieldDetectionResult

--- layer/autonomous/fielddetectionresult.cpp
+++ layer/autonomous/fielddetectionresult.cpp
@@ -1,7 +1,37 @@
 #include "layer/autonomous/fielddetectionresult.h"
+#include "common/compare.h"
 
+using namespace std;
+using namespace RoboHockey::Common;
 using namespace RoboHockey::Layer::Autonomous;
 
-FieldDetectionResult::FieldDetectionResult()
-{ }
+FieldDetectionResult::FieldDetectionResult():
+	m_compare(0.1)
+{
+	m_confirmedPositionsThisSide.reserve(7);
+	m_confirmedPositionsOppositeSide.reserve(7);
+}
+
+RobotPosition FieldDetectionResult::getTransformationDestination() const
+{
+	return m_newOrigin;
+}
+
+bool FieldDetectionResult::isEqualDetectionResult(RobotPosition &position) const
+{
+	return m_compare.isFuzzyEqual(position, m_newOrigin);
+}
+
+void FieldDetectionResult::confirmDetectionResultWithPosition(RobotPosition &position)
+{
+	if (m_compare.isFuzzyEqual(position, m_newOrigin))
+	{
+
+	}
+}
+
+bool FieldDetectionResult::isConfirmedByBothSides() const
+{
+	return m_confirmedPositionsThisSide.size() > (size_t) 2 && m_confirmedPositionsOppositeSide.size() > (size_t) 2;
+}
 
--- layer/autonomous/fielddetectionresult.h
+++ layer/autonomous/fielddetectionresult.h
@@ -1,6 +1,10 @@
 #ifndef ROBOHOCKEY_LAYER_AUTONOMOUS_FIELDDETECTIONRESULT_H
 #define ROBOHOCKEY_LAYER_AUTONOMOUS_FIELDDETECTIONRESULT_H
 
+#include "common/compare.h"
+#include "common/robotposition.h"
+#include <vector>
+
 namespace RoboHockey
 {
 namespace Layer
@@ -11,7 +15,19 @@ class FieldDetectionResult
 	{
 	public:
 		FieldDetectionResult();
-		
+
+		Common::RobotPosition getTransformationDestination() const;
+		bool isEqualDetectionResult(Common::RobotPosition &position) const;
+
+		void confirmDetectionResultWithPosition(Common::RobotPosition &position);
+		bool isConfirmedByBothSides() const;
+
+	private:
+		Common::RobotPosition m_newOrigin;
+		Common::Compare m_compare;
+		std::vector<Common::Point> m_confirmedPositionsThisSide;
+		std::vector<Common::Point> m_confirmedPositionsOppositeSide;
+
 	};
 }
 }
