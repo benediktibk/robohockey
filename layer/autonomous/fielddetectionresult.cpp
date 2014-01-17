38fafdb91951eabbfaec48375480ba1c04debcc2
Kevin Meyer <meyer.kevin@mytum.de>
2014-01-17 14:59:07 +0100

    increased epsilon in FieldDectorResult

--- layer/autonomous/fielddetectionresult.cpp
+++ layer/autonomous/fielddetectionresult.cpp
@@ -21,7 +21,7 @@ RobotPosition FieldDetectionResult::getTransformationDestination() const
 bool FieldDetectionResult::isEqualDetectionResult(RobotPosition &position) const
 {
 	Compare angleCompare(0.5);
-	Compare pointCompare(0.12);
+	Compare pointCompare(0.3);
 	return pointCompare.isFuzzyEqual(position.getPosition(), m_newOrigin.getPosition())
 			&& angleCompare.isFuzzyEqual(position.getOrientation(), m_newOrigin.getOrientation());
 }
