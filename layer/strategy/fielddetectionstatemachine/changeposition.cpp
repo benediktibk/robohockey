ac8e399d400be5ea650ada95a0ceda2eee4a1924
Kevin Meyer <meyer.kevin@mytum.de>
2014-01-26 10:18:31 +0100

    cleaned up ChangePositionState

--- layer/strategy/fielddetectionstatemachine/changeposition.cpp
+++ layer/strategy/fielddetectionstatemachine/changeposition.cpp
@@ -5,13 +5,14 @@
 using namespace std;
 using namespace RoboHockey::Common;
 using namespace RoboHockey::Layer;
+using namespace RoboHockey::Layer::Strategy::Common;
 using namespace RoboHockey::Layer::Strategy::FieldDetectionStateMachine;
 
 ChangePosition::ChangePosition(Autonomous::Robot &robot, Autonomous::Field &field, Strategy::Common::Referee &referee, Logger &logger):
 	State(robot, field, referee, logger, true)
 { }
 
-RoboHockey::Layer::Strategy::Common::State *ChangePosition::nextState()
+State *ChangePosition::nextState()
 {
 	if (m_robot.reachedTarget() || m_robot.cantReachTarget())
 		return new DetectField(m_robot, m_field, m_referee, m_logger, 0);
